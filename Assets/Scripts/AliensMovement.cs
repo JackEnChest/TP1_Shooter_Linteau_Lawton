598fa6c31412f0087e8f52586ee9a73d5c286418
Jacob Linteau <jacob.linteau@hotmail.com>
2021-10-05 12:58:57 -0400

    Intégration du navmesh dans le script du mouvement de l'alien

--- Assets/Scripts/AliensMovement.cs
+++ Assets/Scripts/AliensMovement.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AliensMovement : MonoBehaviour
 {
     private GameObject spaceMarine;
-    [SerializeField] private float speed;
+    [SerializeField] float speed;
+    private NavMeshAgent navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
         spaceMarine = GameObject.Find("SpaceMarine");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        if (navMeshAgent != null)
+        {
+            navMeshAgent.destination = spaceMarine.transform.position;
+        }
     }
 
     // Update is called once per frame
@@ -17,7 +25,10 @@ public class AliensMovement : MonoBehaviour
     {
         if (gameObject.activeSelf)
         {
-
+            if (navMeshAgent == null)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, spaceMarine.transform.position, speed * Time.deltaTime);
+            }
         }
     }
 }
